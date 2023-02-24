4a08a26a7498e73e438783169dd36041e244f57e
Vinícius Martins Correia <vinix.correia@gmail.com>
2023-02-23 21:21:17 -0300

    create migration with user

--- src/infra/database/migration/1677036780759-insert-super-admin.ts
+++ src/infra/database/migration/1677036780759-insert-super-admin.ts
@@ -1,11 +1,18 @@
-import { MigrationInterface, QueryRunner } from "typeorm"
+import { MigrationInterface, QueryRunner } from 'typeorm';
+import { UserEntity } from '../entities/user.entity';
 
 export class insertSuperAdmin1677036780759 implements MigrationInterface {
+	public async up(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.manager.save(
+			queryRunner.manager.create<UserEntity>(UserEntity, {
+				name: 'Super Admin',
+				username: 'admin',
+				password: '12345',
+			})
+		);
+	}
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+	public async down(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.query(`DELETE FROM USER WHERE name = 'Super Admin'`);
+	}
 }

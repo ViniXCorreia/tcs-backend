87ee10ec30ae4ab1dda618b95dbc2d05a730dc99
Matteus <matteus.camargo@gmail.com>
2023-02-23 22:18:13 -0300

    entities

--- src/user/useCases/implementation/find-one.usecase.ts
+++
@@ -1,8 +0,0 @@
-import { UserEntity } from 'src/infra/database/entities/user.entity';
-import { IUseCase } from 'src/_shared/protocols/IUseCase';
-
-export class FindOneUseCase implements IUseCase<number, UserEntity> {
-	execute(data: number): Promise<UserEntity> {
-		throw new Error('Method not implemented.');
-	}
-}
